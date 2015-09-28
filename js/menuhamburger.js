2fbf952dec2a1798d576d290ce8f59ff8e1c3027
Mehdi <mehdi.maizate@gmail.com>
2015-09-28 12:09:42 +0200

    correction2

--- js/menuhamburger.js
+++ js/menuhamburger.js
@@ -14,10 +14,7 @@
   "-ms-transform": "translate3D(-250px, 0, 0)",
   "-webkit-transform": "translate3D(-250px, 0, 0)",
   "transform": "translate3D(-250px, 0, 0)"});
-      /*
-        $('#container').animate({left: "-250px"}, 400).css({"overflow":"hidden"});
-      $('#main-nav').animate({right: "0"}, 400);
-      */
+
       $(this).removeClass('closed').addClass('open').html("<i class='fa fa-times'></i>");
       $('.fade').fadeIn();
     }
@@ -31,10 +28,7 @@
   "-webkit-transform": "translate3D(0px, 0, 0)",
   "transform": "translate3D(0px, 0, 0)"});
 
-      /*
-       $('#container').animate({left: "0"}, 400).css({"overflow":"scroll"});
-      $('#main-nav').animate({right: "-250px"}, 400);
-      */
+
       $(this).removeClass('open').addClass('closed').html('<i class="fa fa-bars"></i>');
       $('.fade').fadeOut();
     }
@@ -43,8 +37,15 @@
 
 //Close nave click a
 $('#main-nav a').click(function() {
-  $('#container').animate({left: "0"}, 400).css({"overflow":"scroll"});
-      $('#main-nav').animate({right: "-250px"}, 400);
+        $('#container').css({  "-moz-transform": "translate3D(0px, 0, 0)",
+  "-ms-transform": "translate3D(0px, 0, 0)",
+  "-webkit-transform": "translate3D(0px, 0, 0)",
+  "transform": "translate3D(0px, 0, 0)"});
+      $('#main-nav').css({  "-moz-transform": "translate3D(0px, 0, 0)",
+  "-ms-transform": "translate3D(0px, 0, 0)",
+  "-webkit-transform": "translate3D(0px, 0, 0)",
+  "transform": "translate3D(0px, 0, 0)"});
+
       $('#navicon').removeClass('open').addClass('closed').html('<i class="fa fa-bars"></i>');
       $('.fade').fadeOut();
   });
